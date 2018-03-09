69d7537bd6af2bb6ddfa2f97de6d0d88452834a7
hanqing <qing.han@bigmarker.com>
2018-03-09 14:57:07 +0800

    test

--- BigmarkerRoom/ViewController.swift
+++ BigmarkerRoom/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let id = CurrentUser.obfuscatedId()
+        print(id)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
