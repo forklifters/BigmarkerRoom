fdca8df0afd9dece36a80b45c8075d66e06825b3
hanqing <qing.han@bigmarker.com>
2018-03-09 16:32:07 +0800

    0.2.2

---
+++ BigmarkerRoom/CurrentUser.swift
@@ -0,0 +1,20 @@
+//
+//  CurrentUser.swift
+//  bigmarker
+//
+//  Created by hanqing on 8/17/16.
+//  Copyright © 2016 hanqing. All rights reserved.
+//
+
+import Foundation
+
+class CurrentUser: NSObject {
+    
+
+    class func name() -> String {
+      print("=============")
+      return "1111"
+    }
+    
+    
+}
